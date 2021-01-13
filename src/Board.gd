39c74ef2efadac6898c5fea7d2f487cc725e5280
andrew-wilkes <github@urgentclick.com>
2021-01-13 15:38:08 +0900

    Add files

---
+++ src/Board.gd
@@ -0,0 +1,79 @@
+extends Control
+
+export var square_width = 64
+export(Color) var white
+export(Color) var grey
+export(Color) var mod_color
+
+const num_squares = 64
+
+var grid : PoolStringArray
+
+func _ready():
+	# grid will map the pieces in the game
+	grid.resize(num_squares)
+	draw_tiles()
+	setup_pieces()
+	#test_is_white()
+	#test_highlight_square()
+	#$Grid.get_child(0).add_child(piece.instance())
+
+
+func setup_pieces():
+	var seq = "PPPPPPPPRNBQKBNRPPPPPPPP"
+	for i in 16:
+		$Grid.get_child(i).add_child(Pieces.get_piece(seq[i + 8], "B"))
+		grid[i] = "B" + seq[i + 8]
+		$Grid.get_child(i + 48).add_child(Pieces.get_piece(seq[i]))
+		grid[i + 48] = "W" + seq[i]
+
+
+func draw_tiles():
+	var white_square = ColorRect.new()
+	white_square.color = white
+	white_square.rect_min_size = Vector2(square_width, square_width)
+	var grey_square = white_square.duplicate()
+	grey_square.color = grey
+	# Add squares to grid
+	var odd = true
+	for n in 8:
+		odd = !odd
+		for m in 8:
+			odd = !odd
+			if odd:
+				$Grid.add_child(white_square.duplicate())
+			else:
+				$Grid.add_child(grey_square.duplicate())
+
+
+func test_highlight_square():
+	for n in num_squares:
+		highlight_square(n)
+		yield(get_tree().create_timer(0.1), "timeout")
+		highlight_square(n, false)
+
+
+func highlight_square(n: int, apply = true):
+	assert(n >= 0)
+	assert(n < num_squares)
+	var sqr: ColorRect = $Grid.get_child(n)
+	if apply:
+		sqr.color = mod_color
+	else:
+		if is_white(n):
+			sqr.color = white
+		else:
+			sqr.color = grey
+
+
+func test_is_white():
+	for n in num_squares:
+		if $Grid.get_child(n).color == white:
+			assert(is_white(n))
+		else:
+			assert(!is_white(n))
+
+
+func is_white(n: int):
+# warning-ignore:integer_division
+	return 0 == ((n / 8) + n) % 2
---
+++ src/Pieces.gd
@@ -0,0 +1,34 @@
+#tool
+
+extends GridContainer
+
+var keys = "BKNPQR"
+
+func get_piece(key = "P", color = "W"):
+	var i = keys.find(key)
+	if color == "W":
+		i += 6
+	var p = get_child(i).duplicate()
+	p.rect_position = Vector2(0, 0)
+	return p
+
+
+func setup():
+	var dir = Directory.new()
+	if dir.open("res://pieces") == OK:
+		var files = []
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if file_name.get_extension() == "png":
+				files.append(file_name)
+			file_name = dir.get_next()
+		files.sort()
+		print(files)
+		var i = 0
+		for file in files:
+			var tb = get_child(i)
+			tb.name = file.get_basename()
+			var img = load("res://pieces/" + file)
+			tb.texture_normal = img
+			i += 1
