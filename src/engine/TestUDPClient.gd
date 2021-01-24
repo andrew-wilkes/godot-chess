76d8c3517b9197e36d6f800fb4c394cd310c086f
andrew-wilkes <github@urgentclick.com>
2021-01-24 12:59:52 +0900

    Complete UDP ping test

--- src/engine/TestUDPClient.gd
+++ src/engine/TestUDPClient.gd
@@ -1,10 +1,21 @@
 extends Control
 
+export var server_exe = ""
+
+var server_pid
+
 func _ready():
-	# Run the server
-	var pid = OS.execute("ping-server", [], false)
-	print("PID of server: %d" % pid)
-	# Send the name of the engine file to the server
+	# Check if server file exists
+	var file = File.new()
+	if !file.file_exists(server_exe):
+		breakpoint
+	file.close()
+	# Start UDP server
+	server_pid = OS.execute(server_exe, [], false)
+	print("PID of server: %d" % server_pid)
+	# Give it time to start
+	yield(get_tree(), "idle_frame")
+	# Set up the UDP client and send a packet
 	$UDPClient.connect_to_server()
 	$UDPClient.send_packet("engine dummy-engine")
 	$Timer.start()
@@ -17,3 +28,8 @@ func _on_UDPClient_got_packet(pkt):
 
 func _on_Timer_timeout():
 	print("Timed out! Check that UDP server is running")
+
+
+func _on_TestUDPClient_tree_exited():
+	if OS.kill(server_pid):
+		print("Failed to kill server process")
