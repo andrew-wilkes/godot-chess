a25642e2409ede5b743c214c8df8391a491e6d5f
andrew-wilkes <github@urgentclick.com>
2021-01-26 15:04:32 +0000

    Add test scene for chess engine

---
+++ src/engine/TestChessEngine.gd
@@ -0,0 +1,45 @@
+extends Control
+
+export var server_exe = ""
+export var engine = ""
+
+var server_pid
+var counter = 0
+
+func _ready():
+	# Check if server file exists
+	var file = File.new()
+	if !file.file_exists(server_exe):
+		breakpoint
+	# Check if engine file exists
+	if !file.file_exists(engine):
+		breakpoint
+	file.close()
+	# Start UDP server
+	server_pid = OS.execute(server_exe, [engine], false)
+	print("PID of server: %d" % server_pid)
+	# Give it time to start
+	#yield(get_tree(), "idle_frame")
+	yield(get_tree().create_timer(2.0), "timeout")
+	# Set up the UDP client and send a packet
+	$UDPClient.set_server()
+	$UDPClient.send_packet("uci")
+	$Timer.start()
+
+
+func _on_UDPClient_got_packet(pkt):
+	$Timer.stop()
+	print(pkt)
+	counter += 1
+	yield(get_tree().create_timer(1.0), "timeout")
+	$UDPClient.send_packet("quit")
+
+
+func _on_Timer_timeout():
+	print("Timed out! Check that UDP server is running")
+	queue_free()
+
+
+func _on_tree_exited():
+	if OS.kill(server_pid):
+		print("Failed to kill server process")
